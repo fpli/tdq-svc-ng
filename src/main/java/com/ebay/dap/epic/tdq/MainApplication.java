fc3354da2eab8b930495fad9e2b6ac28014dc2cd
fangpli <fangpli@ebay.com>
2023-02-01 15:57:53 +0800

    migrate tag feature

--- src/main/java/com/ebay/dap/epic/tdq/MainApplication.java
+++ src/main/java/com/ebay/dap/epic/tdq/MainApplication.java
@@ -1,10 +1,12 @@
 package com.ebay.dap.epic.tdq;
 
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
@@ -12,6 +14,8 @@ import java.util.TimeZone;
 
 @MapperScan("com.ebay.dap.epic.tdq.data.mapper.mybatis")
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class})
+@EnableScheduling
+@EnableSchedulerLock(defaultLockAtMostFor = "PT30S")
 public class MainApplication {
 
     public static void main(String[] args) {
