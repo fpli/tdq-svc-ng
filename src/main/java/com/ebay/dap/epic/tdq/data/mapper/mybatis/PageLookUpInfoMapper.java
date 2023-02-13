2faa332208a80a8db8c778486e6ed1100dd18e09
fangpli <fangpli@ebay.com>
2023-02-13 10:51:31 +0800

    fix empty collection issue for mybatis plugs query

--- src/main/java/com/ebay/dap/epic/tdq/data/mapper/mybatis/PageLookUpInfoMapper.java
+++ src/main/java/com/ebay/dap/epic/tdq/data/mapper/mybatis/PageLookUpInfoMapper.java
@@ -4,16 +4,17 @@ import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.ebay.dap.epic.tdq.data.entity.PageLookUpInfo;
+import org.springframework.util.CollectionUtils;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public interface PageLookUpInfoMapper extends BaseMapper<PageLookUpInfo> {
     default List<PageLookUpInfo> findAllByPageIdIn(List<Integer> pageIds) {
         LambdaQueryWrapper<PageLookUpInfo> lambdaQueryWrapper = Wrappers.lambdaQuery();
-        if (pageIds.isEmpty()) {
-            pageIds = new ArrayList<>(pageIds);
-            pageIds.add(-1);
+        if (CollectionUtils.isEmpty(pageIds)){
+            pageIds = Collections.singletonList(null);
         }
         lambdaQueryWrapper.in(PageLookUpInfo::getPageId, pageIds);
         return selectList(lambdaQueryWrapper);
--- src/main/java/com/ebay/dap/epic/tdq/data/mapper/mybatis/PageUsageMapper.java
+++ src/main/java/com/ebay/dap/epic/tdq/data/mapper/mybatis/PageUsageMapper.java
@@ -4,13 +4,21 @@ import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.ebay.dap.epic.tdq.data.entity.PageUsageEntity;
+import org.springframework.util.CollectionUtils;
 
 import java.time.LocalDate;
+import java.util.Collections;
 import java.util.List;
 
 public interface PageUsageMapper extends BaseMapper<PageUsageEntity> {
     default List<PageUsageEntity> findAllByPageIdInAndDtIn(List<Integer> pageIds, List<LocalDate> dts) {
         LambdaQueryWrapper<PageUsageEntity> lambdaQueryWrapper = Wrappers.lambdaQuery();
+        if (CollectionUtils.isEmpty(pageIds)){
+            pageIds = Collections.singletonList(null);
+        }
+        if (CollectionUtils.isEmpty(dts)){
+            dts = Collections.singletonList(null);
+        }
         lambdaQueryWrapper.in(PageUsageEntity::getPageId, pageIds);
         lambdaQueryWrapper.in(PageUsageEntity::getDt, dts);
         return selectList(lambdaQueryWrapper);
@@ -18,6 +26,9 @@ public interface PageUsageMapper extends BaseMapper<PageUsageEntity> {
 
     default List<PageUsageEntity> findAllByPageIdInAndDtBetween(List<Integer> pageIds, LocalDate startDt, LocalDate endDt) {
         LambdaQueryWrapper<PageUsageEntity> lambdaQueryWrapper = Wrappers.lambdaQuery();
+        if (CollectionUtils.isEmpty(pageIds)){
+            pageIds = Collections.singletonList(null);
+        }
         lambdaQueryWrapper.in(PageUsageEntity::getPageId, pageIds);
         lambdaQueryWrapper.ge(PageUsageEntity::getDt, startDt);
         lambdaQueryWrapper.le(PageUsageEntity::getDt, endDt);
