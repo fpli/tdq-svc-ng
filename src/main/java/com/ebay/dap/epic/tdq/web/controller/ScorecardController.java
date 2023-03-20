6c8ec4dea435454e3f84fc35bc681ac7c565846a
fangpli <fangpli@ebay.com>
2023-03-20 17:36:36 +0800

    ScoreCardController

--- src/main/java/com/ebay/dap/epic/tdq/web/controller/ScorecardController.java
+++ src/main/java/com/ebay/dap/epic/tdq/web/controller/ScorecardController.java
@@ -2,15 +2,15 @@ package com.ebay.dap.epic.tdq.web.controller;
 
 import com.ebay.dap.epic.tdq.data.vo.ScorecardItemVO;
 import com.ebay.dap.epic.tdq.service.ScorecardService;
+import com.ebay.dap.epic.tdq.service.scorecard.ExecutionEngine;
+import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -20,6 +20,17 @@ import java.util.List;
 @Slf4j
 @Tag(name = "Scorecard API", description = "scorecard api")
 public class ScorecardController {
+
+    @Autowired
+    private ExecutionEngine executionEngine;
+
+    @Hidden
+    @PostMapping(path = "/run")
+    public ResponseEntity<Void> run(@RequestBody LocalDate dt) {
+        log.info("Trigger scorecard execution using API");
+
+        return ResponseEntity.ok().build();
+    }
 
     @Autowired
     private ScorecardService scorecardService;
