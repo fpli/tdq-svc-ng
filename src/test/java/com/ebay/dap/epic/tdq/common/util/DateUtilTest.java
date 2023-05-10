22e05e8afc2144392bb876fc01bcb2c806f521f9
fangpli <fangpli@ebay.com>
2023-05-10 16:56:18 +0800

    add date range condition for scorecard detail

--- src/test/java/com/ebay/dap/epic/tdq/common/util/DateUtilTest.java
+++ src/test/java/com/ebay/dap/epic/tdq/common/util/DateUtilTest.java
@@ -4,8 +4,12 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+import java.time.Period;
 import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Stream;
 
 class DateUtilTest {
 
@@ -18,14 +22,35 @@ class DateUtilTest {
 
     @Test
     void datesStepBackBetween() {
-        LocalDate startDate = LocalDate.of(2022, 1, 1);
-        LocalDate endDate = LocalDate.of(2023, 1, 1);
+        LocalDate endDate = LocalDate.now();
+        LocalDate lastYear = endDate.minusYears(1);
         LocalDate sixMonthsAgo = endDate.minusMonths(6);
         LocalDate threeMonthsAgo = endDate.minusMonths(3);
-        List<LocalDate> localDates = DateUtil.datesStepBackBetween(startDate, endDate, 1, ChronoUnit.MONTHS);
-        Assertions.assertThat(localDates).isNotEmpty();
-        Assertions.assertThat(localDates).size().isEqualTo(12);
-        Assertions.assertThat(localDates).contains(endDate.minusMonths(1));
+        long amountToSubtract = 1;
+        List<LocalDate> localDates = DateUtil.datesStepBackBetween(lastYear, endDate, 1, ChronoUnit.MONTHS);
+        System.out.println(localDates);
+        System.out.println(sixMonthsAgo + "---sixMonthsAgo---");
+        System.out.println(DateUtil.datesStepBackBetween(sixMonthsAgo, endDate, 2, ChronoUnit.WEEKS));
+        System.out.println(threeMonthsAgo + "--threeMonthsAgo---");
+        System.out.println(DateUtil.datesStepBackBetween(threeMonthsAgo, endDate, 1, ChronoUnit.WEEKS));
+        System.out.println(DateUtil.datesStepBackBetween(endDate.minusMonths(1), endDate, 1, ChronoUnit.DAYS));
+
+        System.out.println();
+        System.out.println();
+        Stream<LocalDate> localDateStream = DateUtil.datesStepBackBetween(lastYear, endDate, Period.ofMonths(1));
+        System.out.println(localDateStream.toList());
+        System.out.println(DateUtil.datesStepBackBetween(sixMonthsAgo, endDate, Period.ofWeeks(2)).toList());
+        System.out.println(DateUtil.datesStepBackBetween(threeMonthsAgo, endDate, Period.ofWeeks(1)).toList());
+        System.out.println("***************");
+        List<LocalDate> localDateList = DateUtil.datesStepBackBetween(endDate.minusMonths(1), endDate, Period.ofDays(1)).toList();
+
+        ArrayList<LocalDate> localDates1 = new ArrayList<>(localDateList);
+        Iterator<LocalDate> listIterator = localDates1.iterator();
+        while(listIterator.hasNext()){
+            System.out.println(listIterator.next());
+            listIterator.remove();
+        }
+        System.out.println(localDates1);
     }
 
 }
